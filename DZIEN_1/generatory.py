5bcb8e174df8328d6e2dbc93e52842ab37048999
albim72 <marcin.albiniak72@gmail.com>
2023-03-06 11:39:16 +0100

    Update generatory.py

--- DZIEN_1/generatory.py
+++ DZIEN_1/generatory.py
@@ -32,3 +32,27 @@ for i in wznowienie(8,5):
     if i==1001:
         continue
     print(f"wartość z yield: {i}")
+
+
+#przykład 3
+
+def kompleksowy():
+    x = 0
+    while True:
+        print("pierwszy print x1")
+        y = yield x
+        print("drugi print x2")
+        if y is None:
+            x= x+1
+        else:
+            x=y
+
+
+gn = kompleksowy()
+print(next(gn))
+print(next(gn))
+print(next(gn))
+print(gn.send(167))
+print(next(gn))
+print(next(gn))
+print(next(gn))

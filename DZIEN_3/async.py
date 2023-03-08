4415b1e40c4e33296ef74abd7ac88e4609411cd0
albim72 <marcin.albiniak72@gmail.com>
2023-03-08 14:46:17 +0100

    Create async.py

---
+++ DZIEN_3/async.py
@@ -0,0 +1,16 @@
+import asyncio
+import time
+
+async def count():
+    print("początek")
+    await asyncio.sleep(1)
+    print("koniec")
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+if __name__ == '__main__':
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} wykonany w {elapsed:.2f} s")

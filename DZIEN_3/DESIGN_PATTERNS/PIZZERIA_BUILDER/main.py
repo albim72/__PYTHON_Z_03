9c14d58d0c9cc73ae94aa6a4d6a9b44b5d02e370
albim72 <marcin.albiniak72@gmail.com>
2023-03-08 12:12:14 +0100

    Update main.py

--- DZIEN_3/DESIGN_PATTERNS/PIZZERIA_BUILDER/main.py
+++ DZIEN_3/DESIGN_PATTERNS/PIZZERIA_BUILDER/main.py
@@ -4,7 +4,7 @@ import time
 
 PizzaProgress = Enum('PizzaProgress','queued preparation baking ready')
 PizzaDough = Enum('PizzaDough','thin thick')
-PizzaSauce= Enum('PizzaSauce','totmato creme_fraiche')
+PizzaSauce= Enum('PizzaSauce','tomato creme_fraiche')
 PizzaTopping = Enum('PizzaTopping','mozarella double_mozarella bacon ham mushrooms red_onion oregano')
 STEP_DELAY = 3
 

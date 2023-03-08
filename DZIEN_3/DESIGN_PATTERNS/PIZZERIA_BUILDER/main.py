4409c94b5133c27d819571ca55a10fc46add8426
albim72 <marcin.albiniak72@gmail.com>
2023-03-08 11:52:02 +0100

    Update main.py

--- DZIEN_3/DESIGN_PATTERNS/PIZZERIA_BUILDER/main.py
+++ DZIEN_3/DESIGN_PATTERNS/PIZZERIA_BUILDER/main.py
@@ -23,3 +23,37 @@ class Pizza:
         print(f'preparing the {self.dough.name} dough of your {self} ...')
         time.sleep(STEP_DELAY)
         print(f'done with the {self.dough.name} dough')
+        
+        
+class MargaritaBuilder:
+    def __init__(self):
+        self.pizza = Pizza('margarita')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 5
+        
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thin)
+        
+    def add_sauce(self):
+        print("adding the tomato sauce to your margarita...")
+        self.pizza.sauce = PizzaSauce.tomato
+        time.sleep(STEP_DELAY)
+        print('done with the tomato sauce')
+        
+    def add_topping(self):
+        topping_desc = 'double_mozarella oregano'
+        topping_items = (PizzaTopping.double_mozarella, PizzaTopping.oregano)
+        print(f'adding the toping({topping_desc} to your margarita)')
+        self.pizza.topping.append([t for t in topping_items])
+        time.sleep(STEP_DELAY)
+        print(f'done with the topping ({topping_desc}')
+        
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print(f'baking your margarita for {self.baking_time} s')
+        time.sleep(self.baking_time)
+        self.progress = PizzaProgress.ready
+        print('maragrita is ready!!!')
+        
+        

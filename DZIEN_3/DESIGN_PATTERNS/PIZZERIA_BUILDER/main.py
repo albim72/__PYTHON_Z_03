57ad6d1f0d2b0793233781bc2ada3647a93fd67e
albim72 <marcin.albiniak72@gmail.com>
2023-03-08 12:00:47 +0100

    Update main.py

--- DZIEN_3/DESIGN_PATTERNS/PIZZERIA_BUILDER/main.py
+++ DZIEN_3/DESIGN_PATTERNS/PIZZERIA_BUILDER/main.py
@@ -92,3 +92,20 @@ class CreamyBaconBuilder:
         time.sleep(self.baking_time)
         self.progress = PizzaProgress.ready
         print('creamy bacon is ready!!!')
+        
+        
+class Waiter:
+    def __init__(self):
+        self.builder = None
+        
+    def construct_pizza(self,builder):
+        self.builder = builder
+        steps = (builder.prepare_dough,
+                 builder.add_sauce,
+                 builder.add_topping,
+                 builder.bake)
+        [step() for step in steps]
+        
+    @property
+    def pizza(self):
+        return self.builder.pizza

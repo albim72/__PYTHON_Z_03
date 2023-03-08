8e5d96d8a2675ea159806ae35db932819f9372b3
albim72 <marcin.albiniak72@gmail.com>
2023-03-08 11:26:44 +0100

    Create director.py

---
+++ DZIEN_3/DESIGN_PATTERNS/BUILDER/director.py
@@ -0,0 +1,21 @@
+from builder import Builder
+
+class Director:
+    def __init__(self)->None:
+        self._builder = None
+        
+    @property
+    def builder(self)->Builder:
+        return self._builder
+    
+    @builder.setter
+    def builder(self,builder:Builder)->None:
+        self._builder = builder
+        
+    def build_minimal_version_product(self)->None:
+        self.builder.produce_part_a()
+        
+    def build_full_version_product(self)->None:
+        self.builder.produce_part_a()
+        self.builder.produce_part_b()
+        self.builder.produce_part_c()
